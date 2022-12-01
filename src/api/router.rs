861999ff24d145635f2f5189f62f69bc7793fab2
Jerry Wang <freebufer.wang@gmail.com>
2022-12-01 11:41:06 +0800

    refactor: Optimize.

--- src/api/router.rs
+++ src/api/router.rs
@@ -2,21 +2,18 @@ use axum::{routing::get, Router};
 
 use super::{public, resources};
 
-pub fn get_router() -> Router {
-    Router::new().route("/", get(public::hello_world)).nest(
-        "/api",
-        Router::new()
-            .route("/hello", get(public::hello_world))
-            .nest("/res", get_res_router()),
-    )
+pub fn new_router() -> Router {
+    Router::new()
+        .route("/", get(public::hello_world))
+        .nest("/api", Router::new().nest("/res", resources_router()))
 }
 
-fn get_res_router() -> Router {
+fn resources_router() -> Router {
     Router::new()
-        .route("/GetResources", get(resources::api_get_resources))
         .route("/GetDisks", get(resources::api_get_disks))
         .route("/GetNetworks", get(resources::api_get_networks))
         .route("/GetCpu", get(resources::api_get_cpu))
         .route("/GetSystem", get(resources::api_get_system))
         .route("/GetMemory", get(resources::api_get_memory))
+        .route("/GetResources", get(resources::api_get_resources))
 }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use hwspanel_for_rust::infrastructure::APP_CONTENT;
 
 #[tokio::main]
 async fn main() {
-    let router = api::router::get_router();
+    let router = api::router::new_router();
     let config = config::load_config().await;
     let server = format!("{}:{}", config.server().host(), config.server().port());
     APP_CONTENT.set(config);
